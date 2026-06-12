961f285038150ca225e690557efddedd736b1204
varshitha8523 <166120617+varshitha8523@users.noreply.github.com>
2026-06-12 21:40:22 +0530

    playerRoutes.js

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -1,28 +1,19 @@
-// including all routes related to players
 
-// creating instances of express
 
 const express=require('express')
 
-// importing controllers
+
 const {getPlayers, addPlayers, updatePlayer, deletePlayer}=require('../controllers/playerController')
 
-const playerRouter=express.Router()// express.Router() it is a func ,it  helps u to create different routers
+const playerRouter=express.Router()
+playerRouter.get('/get-players',getPlayers)
 
-// getting data of all users
-playerRouter.get('/get-players',getPlayers)// adding getPlayers controller
+playerRouter.post('/add-players',addPlayers)
 
-// posting data of users
 
-playerRouter.post('/add-players',addPlayers)//adding addPlayers controller
-
-// updating req
-
-playerRouter.put('/update-players/:id',updatePlayer)//adding updatePlayer controller and here id and in playercontroller under update heading line below try  ending id  should be same 
-
-// delete req
+playerRouter.put('/update-players/:id',updatePlayer)
 playerRouter.delete('/delete-players/:id',deletePlayer)
 
-// eporting the playerroutes to use in index.js
+
  module.exports=playerRouter;
 
