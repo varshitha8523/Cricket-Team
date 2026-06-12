c4b5c71d07461ab63e6e3492c82cebd9d89b891a
varshitha8523 <166120617+varshitha8523@users.noreply.github.com>
2026-06-12 18:54:48 +0530

    playerModel.js

--- models/playerModel.js
+++ models/playerModel.js
@@ -1,6 +1,6 @@
-// creating model for player in circket project
+
 const mongoose=require('mongoose')
-// ccreating schema for player
+
 const Player=mongoose.Schema({
     first_name:{
         type:String,
@@ -9,7 +9,7 @@ const Player=mongoose.Schema({
     },
     last_name:{
         type:String,
-        required:true//i.e, that field is mandatory
+        required:true
 
     },
     email:{
@@ -31,5 +31,4 @@ const Player=mongoose.Schema({
     }
 })
 
-// exporting this schema to use in index.js
-module.exports=mongoose.model('Player',Player)//before , collection name and after , schema name 
+module.exports=mongoose.model('Player',Player)
